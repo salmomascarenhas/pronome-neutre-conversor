128d9926886239d033f24bdc08950de2f6b2eb0f
Salmo Mascarenhas <salmo.cruz@gmail.com>
2020-09-24 03:05:50 -0300

    all

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Index = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [sucess, setSucess] = useState(false);
     const [inputIsVoid, setInputIsVoid] = useState(false);
-    const [inputText, setInputText] = useState('Tao natural quanto a luz do dia');
+    const [inputText, setInputText] = useState('');
     const [textToConvert, setTextToConvert] = useState({ to_convert: '' });
     const [data, setData] = useState({});
 
@@ -24,21 +24,20 @@ const Index = () => {
         }
         setInputIsVoid(false);
         setTextToConvert({ to_convert });
+        console.log(to_convert)
         try {
             setIsLoading(true);
-            console.log(textToConvert);
             const response = await fetch('https://pronome-neutre-conversor.herokuapp.com/convert', {
                 method: 'POST',
-                mode: 'no-cors',
                 headers: {
                     'Content-type': 'application/json'
                 },
                 body: JSON.stringify(textToConvert)
             });
-            // console.log(response);
-            // const data = await response.json();
-            // setSucess(true);
-            // setData(data);
+            const data = await response.json();
+            console.log(data)
+            setData(data);
+            setSucess(true);
         }
         catch (error) {
             console.log(error);
@@ -84,6 +83,10 @@ const Index = () => {
                 onClick={handleOnClick}>
                 Converter
             </Button>
+            {sucess &&
+                <Text fontSize="2xl">
+                    {data.converted}
+                </Text>}
 
         </Stack>
     );
